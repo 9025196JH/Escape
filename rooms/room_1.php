bd3fbcebee3f0d0ee949173645c44c95bbc9c80f
9025196JH <9025196@student.zadkine.nl>
2026-05-24 17:26:49 +0200

    verhaal
    verhaal gemaakt voor de escape roop en ook in room 1 de namen gewijzigd van de tabel en waar die moet zoeken voor de vraag

--- rooms/room_1.php
+++ rooms/room_1.php
@@ -25,7 +25,7 @@ try {
   <div class="container">
     <?php foreach ($riddles as $index => $riddle) : ?>
     <div class="box box<?php echo $index + 1; ?>" onclick="openModal(<?php echo $index; ?>)"
-      data-index="<?php echo $index; ?>" data-riddle="<?php echo htmlspecialchars($riddle['riddle']); ?>"
+      data-index="<?php echo $index; ?>" data-riddle="<?php echo htmlspecialchars($riddle['question']); ?>"
       data-answer="<?php echo htmlspecialchars($riddle['answer']); ?>">
       Box <?php echo $index + 1; ?>
     </div>
